7930aa647af5309ec26a6962fdf49198a73926c7
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-02 16:42:47 -0600

    Create main.h

---
+++ Chapter09/main.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <set>
+#include <fstream>
+#include <random>
+#include <iomanip>
+
+#include "Book.h"
+#include "Patron.h"
+
+void run_program();
