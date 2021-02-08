d6c1939fbefd6e4a196072c587ad7788df4d5fd1
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-08 12:24:46 -0600

    Update main.h

--- Chapter09/main.h
+++ Chapter09/main.h
@@ -3,20 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
-#include <algorithm>
-#include <functional>
-#include <map>
-#include <set>
-#include <fstream>
-#include <random>
-#include <iomanip>
-#include <unordered_map>
-#include <unordered_set>
 #include <regex>
 
 #include "Book.h"
 #include "Patron.h"
 #include "Library.h"
-
-void run_program();
