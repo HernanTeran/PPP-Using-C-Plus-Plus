247edddd0b4508893ff12f1dfe381d1725810e00
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-09 12:25:00 -0600

    Update Library.cpp

--- Chapter09/Exercise_05_06_07_08_09/Library.cpp
+++ Chapter09/Exercise_05_06_07_08_09/Library.cpp
@@ -25,9 +25,11 @@ namespace Local_Library
 				std::cerr << "This book is already in the library!\n";
 				return;
 			}
-			books.push_back(book);
-			std::cout << book.get_title() << " by " << book.get_author() << " has been added to the library.\n";
 		}
+
+		books.push_back(book);
+		std::cout << book.get_title() << " by " << book.get_author() << " has been added to the library.\n";
+		return;
 	}
 
 	void Library::add_patron(const Patrons::Patron& patron)
@@ -46,27 +48,61 @@ namespace Local_Library
 				std::cerr << "This patron is already in the library!\n";
 				return;
 			}
-			patrons.push_back(patron);
-			std::cout << patron.get_full_name() << " is now in the library.\n";
 		}
+
+		patrons.push_back(patron);
+		std::cout << patron.get_full_name() << " is now in the library.\n";
+		return;
 	}
 
 	void Library::check_out_book(Books::Book& book, Patrons::Patron& patron)
 	{
-		for (const auto& b : books)
+		bool book_match{ true };
+
+		for (std::size_t b{ 0 }; b != books.size(); ++b)
 		{
-			if (b != book)
+			if (books.at(b) == book)
+			{
+				std::cout << book.get_title() << " with ISBN " << book.get_isbn() << " has been found.\n";
+				break;
+			}
+
+			constexpr double last_book{ 1 };
+
+			if (b == books.size() - last_book)
 			{
 				std::cerr << "This book is not in the library!\n";
-				return;
+				book_match = false;
 			}
 		}
 
-		for (const auto& p : patrons)
+		bool patron_match{ true };
+
+		for (std::size_t p{ 0 }; p != patrons.size(); ++p)
 		{
-			if (p != patron)
+			if (patrons.at(p) == patron)
+			{
+				std::cout << patron.get_full_name() << " with account number " 
+						  << patron.get_account_number() << " has been found.\n";
+				break;
+			}
+
+			constexpr double last_patron{ 1 };
+
+			if (p == patrons.size() - last_patron)
 			{
 				std::cerr << "This patron is not in the library!\n";
+				patron_match = false;
+			}
+		}
+
+		if (!book_match || !patron_match) { return; }
+
+		for (const auto& t : transactions)
+		{
+			if (t.book == book && t.patron == patron)
+			{
+				std::cerr << "You have already checked this book out!\n";
 				return;
 			}
 		}
@@ -84,13 +120,17 @@ namespace Local_Library
 		if (book.is_available())
 		{
 			book.check_out();
-			patron.set_fee(1.08);
+			constexpr double base_fee{ 1.08 };
+			patron.set_fee(base_fee);
 			transactions.push_back(Transaction{ book, patron });
+			patrons_in_debt.push_back(patron);
 			std::cout << "Happy Reading " << patron.get_full_name() << "!\n";
+			return;
 		}
 		else
 		{
 			std::cerr << book.get_title() << " by " << book.get_author() << " is currently checked out.\n";
+			return;
 		}
 	}
 
@@ -109,41 +149,64 @@ namespace Local_Library
 				patron.set_fee(zero_balance);
 
 				constexpr std::size_t one_transaction{ 1 };
+
 				if (transactions.size() == one_transaction)
 				{ 
 					transactions.pop_back(); 
+					std::cout << "Transaction has been cleared.\n";
+					return;
 				}
 				else
 				{
 					const std::size_t last_transaction{ transactions.size() - one_transaction };
-					std::swap(t, transactions.at(last_transaction));
-				}
 
-				std::cout << "Transaction has been cleared.\n";
+					std::swap(t, transactions.at(last_transaction));
+					transactions.pop_back();
+					std::cout << "Transaction has been cleared.\n";
+					return;
+				}
+			}
+		}
+
+		for (auto& p : patrons_in_debt)
+		{
+			constexpr std::size_t one_patron{ 1 };
+
+			if (patrons_in_debt.size() == one_patron)
+			{
+				patrons_in_debt.pop_back();
+				std::cout << p.get_full_name() << " has been cleared from in-debt list.\n";
+				return;
+			}
+			else
+			{
+				const std::size_t last_patron{ patrons_in_debt.size() - one_patron };
+
+				std::swap(p, patrons_in_debt.at(last_patron));
+				patrons_in_debt.pop_back();
+				std::cout << p.get_full_name() << " has been cleared from in-debt list.\n";
 				return;
 			}
 		}
+
 		// if there are no matches
 		std::cerr << "There are no matches for that book or patron.\n";
+		return;
 	}
 
-	std::vector<Patrons::Patron> Library::patrons_in_debt(const Library& library)
+	void Library::print_debt_list()
 	{
-		std::vector<Patrons::Patron> patrons;
-
-		if (library.transactions.empty())
+		if (transactions.empty())
 		{
 			std::cerr << "There are currently no transactions.\n";
-			return patrons;
+			return;
 		}
 
-		for (const auto& t : library.transactions)
-		{
-			constexpr double zero_balance{ 0 };
+		std::cout << "\n*PATRONS IN-DEBT*\n"
+			      << "------------------\n";
+		for (const auto& p : patrons_in_debt) { std::cout << p.get_full_name() << '\n'; }
+		std::cout << '\n';
 
-			if (t.patron.get_library_fees() != zero_balance) { patrons.push_back(t.patron); }
-		}
-
-		return patrons;
+		return;
 	}
 }
