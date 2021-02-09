95de2f2628752d9499df07c164aa6992d2141e64
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-09 12:26:51 -0600

    Update main.cpp

--- Chapter09/Exercise_05_06_07_08_09/main.cpp
+++ Chapter09/Exercise_05_06_07_08_09/main.cpp
@@ -2,42 +2,55 @@
 // V1 - 1/26/2021
 // V2 - 2/5/2021
 // V3 - 2/8/2021
-// Excercises 5-7
+// V4 - 2/9/2021
 
 #include "practice.h"
 
-// next time implement a second book and patron
-// make sure there are no bugs when running any of the
-// functions intended to work with only matches
-// implement static variables to replace repetitive variables
-
 int main()
+{
+	run_program();
+
+	return 0;
+}
+
+void run_program()
 {
 	try
 	{
+		// test cases ------------------------------------------------------------------
+		Books::Book rand_book{ "1-2-2-2-F", "The Shining", "Stephen King", 1805, Books::Genre::Biography };
+		Patrons::Patron rand_patron{ "Gene Belcher", 123456 };
+		// ------------------------------------------------------------------------------
+
 		Books::Book book1;
 		std::cin >> book1;
 		std::cout << book1;
 
-		Patrons::Patron patron1{"Bob Belcher", 434323};
+		Patrons::Patron patron1{ "Bob Belcher", 434323 };
 
 		Local_Library::Library library;
 		library.add_book(book1);
 		library.add_patron(patron1);
 		library.check_out_book(book1, patron1);
 
-		std::vector<Patrons::Patron> in_debt = library.patrons_in_debt(library);
+		// test case ------------------------------------------------------------------
+		library.check_out_book(rand_book, rand_patron);
+		// ----------------------------------------------------------------------------
 
-		for (const auto& p : in_debt)
-		{
-			std::cout 
-				<< "\n*patrons in debt*\n"
-				<< "-----------------\n"
-				<< p.get_full_name() << '\n';
-			std::cout << '\n';
-		}
+		Books::Book book2;
+		std::cin >> book2;
+		std::cout << book2;
+
+		Patrons::Patron patron2{ "Linda Belcher", 785342 };
+		library.add_book(book2);
+		library.add_patron(patron2);
+		library.check_out_book(book2, patron2);
+
+		library.print_debt_list();
 
 		library.check_in_book(book1, patron1);
+		library.check_in_book(book2, patron2);
+
 	}
 	catch (Books::Book::Invalid)
 	{
