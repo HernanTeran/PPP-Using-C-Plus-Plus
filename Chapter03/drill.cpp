7f0eaf87ecf53936d26668cf3f9883b81747acf6
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-16 11:57:23 -0600

    Update drill.cpp

--- Chapter03/drill.cpp
+++ Chapter03/drill.cpp
@@ -1,74 +1,70 @@
 #include <iostream>
 #include <string>
+#include <exception>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+
+void error(const std::string& message);
 
 int main()
 {
-	std::cout << "Enter the name of the person you want to write to (followed by 'enter'):\n";
-
-	std::string first_name;
-	getline(std::cin, first_name);
-
-	std::cout
-		<< "Dear " << first_name << ", \n"
-		<< "How are you? I am fine.\n"
-		<< "I miss you.\n\n";
-
-	std::cout << "Enter the name of another friend (followed by 'enter'):\n";
-
-	std::string friend_name;
-	getline(std::cin, friend_name);
-
-	std::cout << "Have you seen " << friend_name << " lately?\n";
-
-	std::cout << "Enter m if the friend is male or an f if the friend is female:\n";
-
-	char friend_sex{ 0 };
-	std::cin.get(friend_sex);
-
-	if (friend_sex == 'm')
+	try
 	{
-		std::cout << "If you see " << friend_name << " please ask him to call me.\n";
-	}
-	else if (friend_sex == 'f')
-	{
-		std::cout << "If you see " << friend_name << " please ask her to call me.\n";
-	}
-	else
-	{
-		std::cout << "Invalid input.\n";
-	}
+		cout << "Enter the name of the person you want to write to:\n";
+		string first_name;
+		cin >> first_name;
+		cout
+			<< "Dear, " << first_name << '\n'
+			<< "How are you? I am fine. I miss you.\n";
 
-	std::cout << "Enter the age of the recipient:\n";
+		cout << "Enter the name of another friend:\n";
+		string friend_name;
+		cin >> friend_name;
 
-	int age{ 0 };
-	std::cin >> age;
+		cout << "Have you seen " << friend_name << " lately?\n";
+		char friend_sex{ '0' };
+		cout << "Enter 'm' for male or 'f' for female:\n";
+		cin >> friend_sex;
 
-	while (age < 0 || age > 110)
-	{
-		std::cout << "You're kidding! Enter the real age!\n";
-		std::cin >> age;
-	}
+		switch (friend_sex)
+		{
+		case 'm':
+			cout << "If you see " << friend_name << " please ask him to call me.\n";
+			break;
+		case 'f':
+			cout << "If you see " << friend_name << " please ask her to call me.\n";
+			break;
+		default:
+			error("unknown character\n");
+			break;
+		}
 
-	std::cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+		cout << "Enter the age of the recipient:\n";
+		int age{ 0 };
+		cin >> age;
+		constexpr int min_age{ 0 }, max_age{ 110 };
 
-	if (age < 12)
-	{
-		std::cout << "Next year you will be " << age + 1 << ".\n";
-	}
-	else if (age == 17)
-	{
-		std::cout << "Next year you will be able to vote.\n";
-	}
-	else if (age >= 70)
-	{
-		std::cout << "I hope you are enjoying retirement.\n";
-	}
-	else
-	{
-		std::cout << "I can't believe how much time has passed.\n";
-	}
+		if (age <= min_age || age >= max_age) { error("you're kidding!\n"); }
 
-	std::cout << "Yours sincerly,\n\n" << "Hernan Teran\n";
+		cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+
+		if (age > 0 && age < 12) { cout << "Next year you will be " << age + 1 << ".\n"; }
+		if (age >= 12 && age < 17) { cout << "You are currently a teenager.\n"; }
+		if (age == 17) { cout << "Next year you will be able to vote.\n"; }
+		if (age >= 18 && age < 70) { cout << "I hope you are enjoying adulthood.\n"; }
+		if (age >= 70) { cout << "I hope you are enjoying retirement.\n"; }
+
+		cout << "Yours sincerely,\n\n" << "Hernan Teran\n";
+	}
+	catch (std::exception& e)
+	{
+		cerr << e.what();
+	}
 
 	return 0;
 }
+
+void error(const std::string& message) { throw std::runtime_error(message); }
