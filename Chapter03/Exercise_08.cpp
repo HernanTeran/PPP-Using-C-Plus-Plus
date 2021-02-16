13a5d0c6a0a84bf27659cac4455f3d3673d5d845
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-16 13:14:49 -0600

    Update Exercise_08.cpp

--- Chapter03/Exercise_08.cpp
+++ Chapter03/Exercise_08.cpp
@@ -1,48 +1,27 @@
+// Hernan Teran 2/16/2021
+// Chapter 3 Exercise 8
+// This is a simple program that outputs whether
+// an input integer is even or odd
+
 #include <iostream>
 
-int get_input();
+using std::cout;
+using std::cin;
+
 void run_program();
-void odd(int val);
-void even(int val);
+
+void run_program()
+{
+	for (int val{ 0 }; cin >> val;)
+	{
+		if (val % 2 == 0) { cout << "The value " << val << " is an even number.\n"; }
+		else { cout << "The value " << val << " is an odd number.\n"; }
+	}
+}
 
 int main()
 {
 	run_program();
 
 	return 0;
-}
-
-int get_input()
-{
-	std::cout << "Enter a value:\n";
-
-	int val{ 0 };
-	std::cin >> val;
-
-	return val;
-}
-
-void run_program()
-{
-	int val1{ 0 };
-	val1 = get_input();
-
-	if (val1 % 2 == 0)
-	{
-		even(val1);
-	}
-	else
-	{
-		odd(val1);
-	}
-}
-
-void odd(int val)
-{
-	std::cout << "The value " << val << " is an odd number.\n";
-}
-
-void even(int val)
-{
-	std::cout << "The value " << val << " is an even number.\n";
 }
