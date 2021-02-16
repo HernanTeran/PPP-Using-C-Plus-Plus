0c0222d4753554289f1c31c52e0e6b90060fa195
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-16 12:29:52 -0600

    Update Exercise_04.cpp

--- Chapter03/Exercise_04.cpp
+++ Chapter03/Exercise_04.cpp
@@ -1,114 +1,78 @@
-#include <iostream>
+// Hernan Teran 2/16/2021
+// Chapter 3 Exercise 4
+// This program computes basic arithmetic operations using
+// two user inputs
 
+#include <iostream>
+#include <string>
+#include <exception>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+
+void error(const std::string& message);
 void run_program();
-int get_input();
-int get_smaller(int val1, int val2);
-int get_larger(int val1, int val2);
-int get_sum(int val1, int val2);
-int get_difference(int val1, int val2);
-int get_product(int val1, int val2);
-double get_ratio(int val1, int val2);
-void display_results(int sm, int lg, int su, int df, int pd, double rt);
+
+int get_number(const std::string& prompt);
+int get_smaller(int v1, int v2);
+int get_larger(int v1, int v2);
+int get_sum(int v1, int v2);
+int get_difference(int v1, int v2);
+int get_product(int v1, int v2);
+double get_ratio(int v1, int v2);
+
+void run_program()
+{
+	try
+	{
+		int val1{ 0 }, val2{ 0 };
+		val1 = get_number("Enter an integer value:\n");
+		val2 = get_number("Enter an integer value:\n");
+
+		int smaller = get_smaller(val1, val2);
+		int larger = get_larger(val1, val2);
+		int sum = get_sum(val1, val2);
+		int difference = get_difference(val1, val2);
+		int product = get_product(val1, val2);
+		double ratio = get_ratio(val1, val2);
+
+		cout
+			<< "smaller: " << smaller << '\n'
+			<< "larger: " << larger << '\n'
+			<< "sum: " << sum << '\n'
+			<< "difference: " << difference << '\n'
+			<< "product: " << product << '\n'
+			<< "ratio: " << ratio << '\n';
+	}
+	catch (std::exception& e)
+	{
+		cerr << e.what();
+	}
+}
+
+int get_number(const std::string& prompt)
+{
+	cout << prompt;
+	int temp{ 0 };
+	cin >> temp;
+	if (!cin) { throw std::runtime_error("error"); }
+	return temp;
+}
+
+int get_smaller(int v1, int v2) { return std::min(v1, v2); }
+int get_larger(int v1, int v2) { return std::max(v1, v2); }
+int get_sum(int v1, int v2) { return v1 + v2; }
+int get_difference(int v1, int v2) { return abs(v1 - v2); }
+int get_product(int v1, int v2) { return v1 * v2; }
+double get_ratio(int v1, int v2) { return (double)get_larger(v1, v2) / (double)get_smaller(v1, v2); }
+
+void error(const std::string& message) { throw std::runtime_error(message); }
 
 int main()
 {
 	run_program();
 
 	return 0;
-}
-
-int get_input()
-{
-	std::cout << "Enter a value:\n";
-
-	int val{ 0 };
-	std::cin >> val;
-
-	return val;
-}
-
-void run_program()
-{
-	int val1{ 0 };
-	val1 = get_input();
-
-	int val2{ 0 };
-	val2 = get_input();
-
-	int smaller{ 0 };
-	smaller = get_smaller(val1, val2);
-
-	int larger{ 0 };
-	larger = get_larger(val1, val2);
-
-	int sum{ 0 };
-	sum = get_sum(val1, val2);
-
-	int difference{ 0 };
-	difference = get_difference(val1, val2);
-
-	int product{ 0 };
-	product = get_product(val1, val2);
-
-	double ratio{ 0 };
-	ratio = get_ratio(val1, val2);
-
-	display_results(smaller, larger, sum, difference, product, ratio);
-}
-
-int get_smaller(int val1, int val2)
-{
-	if (val1 == val2)
-	{
-		return val1;
-	}
-	return val1 < val2 ? val1 : val2;
-}
-
-int get_larger(int val1, int val2)
-{
-	if (val1 == val2)
-	{
-		return val1;
-	}
-	return val1 > val2 ? val1 : val2;
-}
-
-int get_sum(int val1, int val2)
-{
-	return val1 + val2;
-}
-
-int get_difference(int val1, int val2)
-{
-	return val1 > val2 ? val1 - val2 : val2 - val1;
-}
-
-int get_product(int val1, int val2)
-{
-	return val1 * val2;
-}
-
-double get_ratio(int val1, int val2)
-{
-	double d_val1{ val1 + 0.0 };
-	double d_val2{ val2 + 0.0 };
-
-	if (d_val1 > d_val2)
-	{
-		
-		return d_val1 / d_val2;
-	}
-	return d_val2 / d_val1;
-}
-
-void display_results(int sm, int lg, int su, int df, int pd, double rt)
-{
-	std::cout
-		<< "smaller: " << sm << '\n'
-		<< "larger: " << lg << '\n'
-		<< "sum: " << su << '\n'
-		<< "difference: " << df << '\n'
-		<< "product: " << pd << '\n'
-		<< "ratio: " << rt << '\n';
 }
