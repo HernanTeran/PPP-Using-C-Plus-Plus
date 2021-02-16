ab769282719cd42f9cf6602b05833e1f221be659
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-16 13:04:36 -0600

    Update Exercise_06.cpp

--- Chapter03/Exercise_06.cpp
+++ Chapter03/Exercise_06.cpp
@@ -1,58 +1,63 @@
-#include <iostream>
+// Hernan Teran 2/16/2021
+// Chapter 3 Exercise 6
+// This program gets three user-input integers,
+// sorts them a <= b <= c
+// then prints them as a, b, c
 
+#include <iostream>
+#include <string>
+#include <exception>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+
+void error(const std::string& message);
 void run_program();
-double get_input();
+
+void run_program()
+{
+	try
+	{
+		int val1{ 0 }, val2{ 0 }, val3{ 0 };
+		cin >> val1 >> val2 >> val3;
+		if (!cin) { throw std::runtime_error("error"); }
+		
+		double first{ 0 }, second{ 0 }, third{ 0 };
+
+		if (val1 <= val2 && val1 <= val3)
+		{
+			first = val1;
+			second = val2 <= val3 ? val2 : val3;
+			third = second == val2 ? val3 : val2;
+		}
+		else if (val2 <= val1 && val2 <= val3)
+		{
+			first = val2;
+			second = val1 <= val3 ? val1 : val3;
+			third = second == val1 ? val3 : val1;
+		}
+		else
+		{
+			first = val3;
+			second = val2 <= val1 ? val2 : val1;
+			third = second == val2 ? val1 : val2;
+		}
+
+		std::cout << first << ", " << second << ", " << third << '\n';
+	}
+	catch (std::exception& e)
+	{
+		cerr << e.what();
+	}
+}
+
+void error(const std::string& message) { throw std::runtime_error(message); }
 
 int main()
 {
 	run_program();
 
 	return 0;
-}
-
-double get_input()
-{
-	std::cout << "Enter a value:\n";
-
-	double val{ 0 };
-	std::cin >> val;
-
-	return val;
-}
-
-void run_program()
-{
-	double val1{ 0 };
-	val1 = get_input();
-
-	double val2{ 0 };
-	val2 = get_input();
-
-	double val3{ 0 };
-	val3 = get_input();
-	
-	double first{ 0 };
-	double second{ 0 };
-	double third{ 0 };
-	
-	if (val1 <= val2 && val1 <= val3)
-	{
-		first = val1;
-		second = val2 <= val3 ? val2 : val3;
-		third = second == val2 ? val3 : val2;
-	}
-	else if (val2 <= val1 && val2 <= val3)
-	{
-		first = val2;
-		second = val1 <= val3 ? val1 : val3;
-		third = second == val1 ? val3 : val1;
-	}
-	else
-	{
-		first = val3;
-		second = val2 <= val1 ? val2 : val1;
-		third = second == val2 ? val1 : val2;
-	}
-
-	std::cout << first << ", " << second << ", " << third << '\n';
 }
