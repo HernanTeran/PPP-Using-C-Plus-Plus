9a8ac8082c3a15e5458cce5868a32e4bab86accd
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-16 12:14:16 -0600

    Update Exercise_01.cpp

--- Chapter03/Exercise_01.cpp
+++ Chapter03/Exercise_01.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+// Hernan Teran 2/16/2021
+// Chapter 3 Exercise 2
+// This program converts miles to kilometers
 
-double get_miles();
+#include <iostream>
+#include <string>
+#include <exception>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+
+void error(const std::string& message);
+double get_miles(const std::string& prompt, const std::string& message);
 double convert_mi_to_km(double miles);
 void run_program();
 
@@ -11,12 +23,33 @@ int main()
 	return 0;
 }
 
-double get_miles()
+void run_program()
 {
-	std::cout << "Enter miles to convert to kilometers:\n";
+	try
+	{
+		double miles{ 0 };
+		miles = get_miles("Enter miles to convert to kilometers:\n", "error input cannot be less than 0\n");
 
+		double mi_to_km{ 0 };
+		mi_to_km = convert_mi_to_km(miles);
+
+		std::cout
+			<< "Miles: " << miles << " mi\n"
+			<< "Kilometers " << mi_to_km << " km\n";
+	}
+	catch (std::exception& e)
+	{
+		cerr << e.what();
+	}
+}
+
+double get_miles(const std::string& prompt, const std::string& message)
+{
+	cout << prompt;
 	double miles{ 0 };
 	std::cin >> miles;
+
+	if (miles < 0) { throw std::runtime_error(message); }
 
 	return miles;
 }
@@ -28,15 +61,4 @@ double convert_mi_to_km(double miles)
 	return miles * kilometers;
 }
 
-void run_program()
-{
-	double miles{ 0 };
-	miles = get_miles();
-
-	double mi_to_km{ 0 };
-	mi_to_km = convert_mi_to_km(miles);
-
-	std::cout
-		<< "Miles: " << miles << '\n'
-		<< "Kilometers " << mi_to_km << '\n';
-}
+void error(const std::string& message) { throw std::runtime_error(message); }
