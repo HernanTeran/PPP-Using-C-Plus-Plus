586a38e1cb8fa00070ed7eda3c0c96e9c7587cc5
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-19 10:09:52 -0600

    Create main.h

---
+++ Chapter09/Exercise_15_16/main.h
@@ -0,0 +1,17 @@
+// Hernan Teran 2/19/2021
+// Chapter 9 Exercises 15 and 16
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <exception>
+
+#include "Money.h"
+
+void run_program();
