f7a7e7db911019f51f57b432ef4d7e5f0cb5aa7b
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-19 17:29:07 -0600

    Create main.h

---
+++ Chapter21/Exercise_09_10/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <functional>
+#include <exception>
+
+#include "Order.h"
+#include "Purchase.h"
+#include "Order_file.h"
+
+void run_program();
