4ed2becf4cb91f992b42c682929a30d6eed41d6c
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-18 19:26:02 -0600

    Create Money.h

---
+++ Chapter09/Exercise_14/Money.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+namespace Currency
+{
+	class Money
+	{
+	public:
+		// exception class
+		class Invalid_amount{};
+
+		// default constructor
+		Money();
+
+		// constructor
+		Money(long int cents_);
+
+		// nonmodifying operation
+		long int get_cents() const { return cents; }
+		double get_monetary_value() const;
+		double get_rounded_value() const;
+
+	private:
+		long int cents{ 0 };
+	};
+
+	// helper functions
+	bool is_valid_amount(long int cents_);
+
+	std::ostream& operator<<(std::ostream& os, const Money& money);
+	std::istream& operator>>(std::istream& is, Money& money);
+}
