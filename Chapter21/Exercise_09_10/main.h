bc2e45399e6b06040bf5684ce5988420a8fd6e08
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-20 11:52:30 -0600

    Update main.h

--- Chapter21/Exercise_09_10/main.h
+++ Chapter21/Exercise_09_10/main.h
@@ -4,12 +4,20 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <cmath>
 #include <algorithm>
-#include <functional>
+#include <iterator>
+#include <map>
+#include <set>
+#include <fstream>
+#include <random>
+#include <iomanip>
+#include <regex>
 #include <exception>
 
 #include "Order.h"
 #include "Purchase.h"
 #include "Order_file.h"
+#include "Order_data.h"
 
 void run_program();
