0c0a86b01fc197172f0d599fa5ef701bbc8b2d1d
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-27 09:45:09 -0600

    Add files via upload

---
+++ Chapter09/Exercise_02/Main.h
@@ -0,0 +1,32 @@
+﻿// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Main
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 9 Exercise 2 & 3
+//
+// Author: Hernan Teran
+// Created: 2021/02/09
+//
+/*
+* Design and implement a Name_Pairs class holding (name, age) pairs where name is a string and age is a double.
+* Represent that as a vector<string> (called name) and a vector<double> (called age) member.
+* Provide an input operation read_names() that reads a series of names.
+* Provide a read_ages() operation that prompts the user for an age for each name.
+* Provide a print() operation that prints out the (name[i], age[i]) pairs (one per line)
+* in the order determined by the name vector.
+* Provide a sort() operation that sorts the vector in alphabetical order and reorganizes the age vector to match. 
+* Implement all "operations" as member functions.
+* Replace Name_Pair::print() with a global operator << and define == and != for Name_Pairs.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef MAIN_H
+#define MAIN_H
+#pragma once
+
+#include "Name_Pair.h"
+
+#include <iostream>
+
+void driver();
+
+#endif // MAIN_H
