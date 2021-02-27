9842c46c7f0ed704fb591f1fd8c330afe917464f
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-27 13:14:27 -0600

    Update and rename main.cpp to Main.cpp

---
+++ Chapter21/Exercise_09_10/Main.cpp
@@ -0,0 +1,31 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Main
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 21 Exercises 9 & 10
+//
+// Author: Hernan Teran
+// Created: 2021/02/09
+//
+/*
+* Define an Order class with (customer) name, address, data, and vector<Purchase> members.
+* Purchase is a class with a (product) name, unit_price, and count members.
+* Define a mechanism for reading and writing Orders to and from a file.
+* Define a mechanism for printing Orders.
+* Create a file out of at least ten Orders, read it into a vecotr<Order>, sort it by name
+* (of customer), and write it back out to a file.
+* Create another file of at least ten Orders of which about a third are the same as in the first file,
+* read it into a list<Order>, sort it by address (of customer), and write it back out to a file.
+* Merge the two files into a third using std::merge().
+* Compute the total value of the orders in the two files.
+* The value of an individual Purchase is its unit_price * count.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#include "Main.h"
+
+int main()
+{
+	Driver::driver();
+
+	return 0;
+}
