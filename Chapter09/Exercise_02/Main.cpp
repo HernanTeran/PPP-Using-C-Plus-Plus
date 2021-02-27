fe4b71e06e67e80e8e2a5fbed26d6dc4c7c7fa26
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-27 09:44:40 -0600

    Update and rename main.cpp to Main.cpp

---
+++ Chapter09/Exercise_02/Main.cpp
@@ -0,0 +1,47 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Main
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 9 Exercises 2 & 3
+//
+// Author: Hernan Teran
+// Created: 2021/02/09
+//
+/*
+* Design and implement a Name_Pairs class holding (name, age) pairs where name is a string and age is a double.
+* Represent that as a vector<string> (called name) and a vector<double> (called age) member.
+* Provide an input operation read_names() that reads a series of names.
+* Provide a read_ages() operation that prompts the user for an age for each name.
+* Provide a print() operation that prints out the (name[i], age[i]) pairs (one per line)
+* in the order determined by the name vector.
+* Provide a sort() operation that sorts the vector in alphabetical order and reorganizes the age vector to match.
+* Implement all "operations" as member functions.
+* Replace Name_Pair::print() with a global operator << and define == and != for Name_Pairs.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#include "Main.h"
+
+//----------------------------------------------------------------------------------------------------------------------------------
+// void driver()
+//----------------------------------------------------------------------------------------------------------------------------------
+/*
+* @note This function runs the program and is called by main.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+void driver()
+{
+	Name_pairs::Name_pair pairs;
+
+	pairs.read_names("Please enter names ('stop' to terminate):\n");
+	pairs.read_ages("Please enter ages according to name entered ('|' to terminate):\n");
+	pairs.print();
+	pairs.sort();
+	pairs.print();
+}
+
+int main()
+{
+	driver();
+
+	return 0;
+}
