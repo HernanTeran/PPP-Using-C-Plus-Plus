a897b6cefd275231e4e51222ccdede9d13b6b7ed
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-27 12:47:18 -0600

    Update Order_file.h

--- Chapter21/Exercise_09_10/Order_file.h
+++ Chapter21/Exercise_09_10/Order_file.h
@@ -1,7 +1,27 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Order_File
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 21 Exercises 9 & 10
+//
+// Author: Hernan Teran
+// Created: 2021/02/09
+//
+/*
+* Define a mechanism for reading and writing Orders to and from a file.
+* Create a file out of at least ten Orders, read it into a vecotr<Order>, sort it by name
+* (of customer), and write it back out to a file.
+* Create another file of at least ten Orders of which about a third are the same as in the first file,
+* read it into a list<Order>, sort it by address (of customer), and write it back out to a file.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef ORDER_FILE_H
+#define ORDER_FILE_H
 #pragma once
 
 #include "Order.h"
 #include "Purchase.h"
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -9,34 +29,33 @@
 #include <string>
 #include <regex>
 
-namespace Order_files
+namespace Order_Files
 {
-	class Order_file
+	class Order_File
 	{
 	public:
 		// exception class
-		class Invalid_file{};
+		class Invalid_file {};
 
 		// default constructor
-		Order_file();
+		Order_File();
 
 		// constructor
-		Order_file(const std::string& f_name_);
+		Order_File(const std::string& f_name_);
 
-		// write file
+		//--------------------------------------------------------------
+		// Public member functions
+		//--------------------------------------------------------------
 		void write_file(const Orders::Order& order);
-
-		// read file and store into vector
 		void read_file(const Orders::Order& order, std::vector<Orders::Order>& orders);
-
-		// read file and store into list
 		void read_file(const Orders::Order& order, std::list<Orders::Order>& orders);
 
 	private:
 		std::string f_name;
 	};
 
-	// helper functions
 	bool is_valid_f_name(const std::string& f_name);
-	std::istream& operator>>(std::istream& is, Order_file& order_file);
+	std::istream& operator>>(std::istream& is, Order_File& order_file);
 }
+
+#endif // ORDER_FILE_H
