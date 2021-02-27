a6814ba9d568d137674bbfe65e9f95c829ce976f
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-02-27 10:48:29 -0600

    Update Patron.h

--- Chapter09/Exercise_05_06_07_08_09/Patron.h
+++ Chapter09/Exercise_05_06_07_08_09/Patron.h
@@ -1,4 +1,23 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Patron
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 9 Exercises 5 - 9
+//
+// Author: Hernan Teran
+// Created: 2021/02/09
+//
+/*
+* Create a Patron class for the library.
+* The class will have a user's name, library card number, and library fees (if owed).
+* Have functions that access this data, as well as a function to set the fee of the user.
+* Have a helper function that returns a Boolean (bool) depending on whether or not the user owes a fee.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef PATRON_H
+#define PATRON_H
 #pragma once
+
 #include <string>
 
 namespace Patrons
@@ -6,21 +25,29 @@ namespace Patrons
 	class Patron
 	{
 	public:
-		class Invalid{};
+		class Invalid_Patron{};
 
 		Patron() = delete;
 
 		Patron(const std::string& full_name_, int account_number_);
 
-		// nonmodifying operations
-		std::string get_full_name() const { return full_name; }
-		int get_account_number() const { return account_number; }
-		double get_library_fees() const { return library_fee; }
+		//-----------------------------------------------------
+		// Public member functions
+		//-----------------------------------------------------
 
-		// modifying operation
-		void set_fee(double library_fee_)
+		//-----------------------------------------------------
+		// Nonmodifying operations
+		//-----------------------------------------------------
+		inline std::string get_full_name() const { return full_name; }
+		inline int get_account_number() const { return account_number; }
+		inline double get_library_fees() const { return library_fee; }
+
+		//-----------------------------------------------------
+		// Modifying operation
+		//-----------------------------------------------------
+		inline void set_fee(double library_fee_)
 		{
-			if (library_fee_ < 0) { throw Invalid{}; }
+			if (library_fee_ < 0) { throw Invalid_Patron{}; }
 			library_fee = library_fee_;
 		}
 
@@ -30,10 +57,11 @@ namespace Patrons
 		double library_fee{ 0 };
 	};
 
-	// helper functions
 	bool is_valid_patron(const std::string& full_name_, int account_number_);
 	bool owes_fees(const Patron& patron);
 
 	bool operator == (const Patron& obj1, const Patron& obj2);
 	bool operator != (const Patron& obj1, const Patron& obj2);
 }
+
+#endif // PATRON_H
