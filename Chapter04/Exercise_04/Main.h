da4257ed140cbfe197190a41618518174ae6a8e2
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-17 09:01:09 -0500

    Create Main.h

---
+++ Chapter04/Exercise_04/Main.h
@@ -0,0 +1,29 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Main
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 4 Excercise 5
+//
+// Author: Hernan Teran
+// Created: 2021/03/17
+//
+/*
+* Write a program that performs a very simple calculator.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef MAIN_H
+#define MAIN_H
+#pragma once
+
+#include "Calculator.h"
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <random>
+
+char cont_calc(const std::string& prompt);
+void driver();
+
+#endif // MAIN_H
