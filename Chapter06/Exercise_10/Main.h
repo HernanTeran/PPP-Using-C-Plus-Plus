93afb35bc1294f8d3d97ec7739eaf33eb53a3d7e
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-18 15:38:57 -0500

    Create Main.h

---
+++ Chapter06/Exercise_10/Main.h
@@ -0,0 +1,24 @@
+// $Header$
+//----------------------------------------------------------------------------------------------------------------------------------
+//                                               Main
+//----------------------------------------------------------------------------------------------------------------------------------
+// Chapter 6 Excercise 10
+//
+// Author: Hernan Teran
+// Created: 2021/03/18
+//
+/*
+* Write a program that overloads the ! operator to solve permutations and combinations.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+#ifndef MAIN_H
+#define MAIN_H
+#pragma once
+
+#include "Factorial.h"
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#endif // MAIN_H
