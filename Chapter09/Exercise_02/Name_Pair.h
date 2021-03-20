93798d9cffb139ed34820c573b17abec0815a78f
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-20 13:03:45 -0500

    Update Name_Pair.h

--- Chapter09/Exercise_02/Name_Pair.h
+++ Chapter09/Exercise_02/Name_Pair.h
@@ -5,7 +5,7 @@
 // Chapter 9 Exercises 2 & 3
 //
 // Author: Hernan Teran
-// Created: 2021/02/09
+// Created: 2021/03/20
 //
 /*
 * Design and implement a Name_Pairs class holding (name, age) pairs where name is a string and age is a double.
@@ -19,29 +19,44 @@
 * Replace Name_Pair::print() with a global operator << and define == and != for Name_Pairs.
 */
 //----------------------------------------------------------------------------------------------------------------------------------
-#ifndef NAME_PAIR_H
-#define NAME_PAIR_H
+#ifndef NAME_PAIRS_H
+#define NAME_PAIRS_H
 #pragma once
 
+#include <vector>
 #include <string>
 #include <iostream>
-#include <vector>
 
-namespace Name_pairs
+class Name_Pairs
 {
-	class Name_pair
-	{
-	public:
-		// default constructor is fine
+public:
+	// exception class
+	class Invalid_Pair{};
+	
+	// nonmodifying operations
+	size_t get_sz() const { return DATA_SZ; }
+	std::vector<std::string> get_names() const { return name; }
+	std::vector<int> get_ages() const { return age; }
+	
+	// modifying operations
+	void read_names(const std::string& prompt);
+	void read_ages(const std::string& prompt);
+	void sort();
+	void print(const std::string& prompt);
 
-		void read_names(const std::string& prompt);
-		void read_ages(const std::string& prompt);
-		void print();
-		void sort();
-	private:
-		std::vector<std::string> names;
-		std::vector<double> ages;
-	};
-}
+protected:
+	size_t DATA_SZ{ 0 };
+	size_t data_sz(const std::string& prompt);
 
-#endif // NAME_PAIR_H
+private:
+	std::vector<std::string> name;
+	std::vector<int> age;
+};
+
+// helper functions
+std::ostream& operator<<(std::ostream& os, const Name_Pairs& pairs);
+
+bool operator==(const Name_Pairs& lp, const Name_Pairs& rp);
+bool operator!=(const Name_Pairs& lp, const Name_Pairs& rp);
+
+#endif // NAME_PAIRS_H
