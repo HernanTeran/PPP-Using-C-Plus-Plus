b645d9e851ac34e05b2245d973b3a28bc0538040
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-21 12:32:53 -0500

    Update Exercise_01.cpp

--- Chapter10/Exercise_01.cpp
+++ Chapter10/Exercise_01.cpp
@@ -1,94 +1,52 @@
-// Hernan Teran 2/18/2021
+// Hernan Teran 2021/03/21
 // Chapter 10 Exercise 1
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
 
-using std::string;
-using std::vector;
-using std::ofstream;
-using std::ifstream;
-using std::cout;
-using std::cin;
+using namespace std;
 
-// --------------------------------------------------------------------------------------------
-
-struct Numbers
-{
-	vector<double> numbers;
-};
-
-void fill(const string& prompt, Numbers& v)
-{
-	cout << prompt;
-	while (true)
-	{
-		double n{ 0 };
-		if (!(cin >> n)) { error("invalid input\n"); }
-		if (v.numbers.size() == 7) { break; }
-		v.numbers.push_back(n);
-	}
-}
-
-void create_file(const string& prompt, const Numbers& v)
-{
-	cout << prompt;
-	string oname;
-	cin >> oname;
-	ofstream ofs{ oname };
-	if (!ofs) { error("error while creating file, " + oname + '\n'); }
-	for (const auto& num : v.numbers) { ofs << num << ' '; }
-}
-
-void read(const string& prompt, Numbers& v)
-{
-	cout << prompt;
-	string iname;
-	cin >> iname;
-	ifstream ifs{ iname };
-	if (!ifs) { error("cannot open file, " + iname + '\n'); }
-	for (double i{ 0 }; ifs >> i;) { v.numbers.push_back(i); }
-}
-
-void print_sum(const Numbers& v)
-{
-	double sum{ 0 };
-	for (const auto& n : v.numbers) { sum += n; }
-	cout << "[sum: " << sum << "]\n";
-}
-
-// --------------------------------------------------------------------------------------------
-
-void error(const string& message) { throw std::runtime_error(message); }
-
-void run_program()
-{
-	try
-	{
-		Numbers numbers;
-		fill("Please input numbers:\n", numbers);
-		create_file("Please enter the output file name:\n", numbers);
-
-		Numbers new_numbers;
-		read("Please enter the input file name:\n", new_numbers);
-		print_sum(new_numbers);
-
-	}
-	catch (std::exception& e)
-	{
-		cerr << e.what();
-	}
-	catch (...)
-	{
-		cerr << "something went wrong\n";
-	}
-}
+void error(string s1, string s2);
 
 int main()
 {
-	run_program();
+	try
+	{
+		cout << "Please enter the name of output file: ";
+		string oname;
+		cin >> oname;
+		ofstream ofs{ oname };
+		if (!ofs) error("can't open output file ", oname);
+
+		for (int i{ 0 }; i != 100; ++i)
+			ofs << i << ' ';
+
+		ofs.close();
+
+		cout << "Please enter the input file name: ";
+		string iname;
+		cin >> iname;
+		ifstream ifs{ iname };
+		if (!ifs) error("can't open input file ", iname);
+
+		int sum{ 0 };
+
+		for (int i{ 0 }; ifs >> i;)
+			sum += i;
+
+		cout << "sum: " << sum << endl;
+	}
+	catch (exception& e)
+	{
+		cerr << e.what();
+	}
+
 
 	return 0;
+}
+
+void error(string s1, string s2)
+{
+	throw runtime_error(s1 + s2);
 }
