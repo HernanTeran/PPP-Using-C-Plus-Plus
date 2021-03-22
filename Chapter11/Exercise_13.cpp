743b7b1ced9210ca8d205b6ed7833d944632621c
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-22 15:43:58 -0500

    Update Exercise_13.cpp

--- Chapter11/Exercise_13.cpp
+++ Chapter11/Exercise_13.cpp
@@ -3,32 +3,29 @@
 #include <vector>
 #include <fstream>
 
-using std::cout;
-using std::string;
-using std::cin;
-using std::fstream;
-using std::ifstream;
-using std::vector;
+using namespace std;
 
 int main()
 {
-	// file path: practice -> out -> build -> x64 -> practice
-
 	cout << "Please enter the input file name:\n";
 	string iname;
 	cin >> iname;
 	ifstream ifs{ iname };
+	if (!ifs)
+		cerr << "can't open file, " << iname << '\n';
+	
 	vector<string> separated;
 
 	for (string temp; getline(ifs, temp);)
 	{
 		string word;
 
-		for (std::size_t i{ 0 }; i < temp.length(); ++i)
+		for (size_t i{ 0 }; i < temp.length(); ++i)
 		{
-			if (!isspace(temp.at(i))) { word += temp.at(i); }
+			if (!isspace(temp.at(i)))
+				word += temp.at(i);
 
-			const std::size_t end_pos{ temp.length() - 1 };
+			const size_t end_pos{ temp.length() - 1 };
 
 			if (isspace(temp.at(i)) || i == end_pos)
 			{
@@ -38,9 +35,10 @@ int main()
 		}
 	}
 
-	std::sort(separated.rbegin(), separated.rend());
+	sort(separated.rbegin(), separated.rend());
 
-	for (const auto& word : separated) { cout << word << ' '; }
+	for (const auto& word : separated)
+		cout << word << ' ';
 
 	return 0;
 }
