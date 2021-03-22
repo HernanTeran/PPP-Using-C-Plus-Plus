08d4af310844dd2221fa71b0606552b3c2898873
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-22 15:35:16 -0500

    Update Exercise_02.cpp

--- Chapter11/Exercise_02.cpp
+++ Chapter11/Exercise_02.cpp
@@ -4,42 +4,31 @@
 #include <fstream>
 #include <regex>
 
-using std::cout;
-using std::string;
-using std::cin;
-using std::fstream;
-using std::ifstream;
-using std::ofstream;
-using std::cerr;
-using std::vector;
-using std::regex;
+using namespace std;
 
 // *READ*
-// the regex pattern help finally solve this exercise
-// use it to find a word/phrase in between
-// the boundaries of other words
+// the regex pattern helped finally solve this exercise
+// use it to find a word/phrase in between the boundaries of other words
 
 int main()
 {
-	// file path: practice -> out -> build -> x64 -> practice
-
 	cout << "Please enter the input file name:\n";
 	string iname;
 	cin >> iname;
 	ifstream ifs{ iname };
-	if (!ifs) { cerr << "error cannot open file name, " << iname << '\n'; }
+	if (!ifs)
+		cerr << "error cannot open file name, " << iname << '\n';
 
+	string word_to_find{ "time" };
+	regex pat{ "\\b" + word_to_find + "\\b" };
 	vector<string> matches;
 
-	const string word_to_find{ "time" };
-	regex pat{ "\\b" + word_to_find + "\\b" };
-
 	for (string line; getline(ifs, line); )
-	{
-		if (regex_search(line, pat)) { matches.push_back(line); }
-	}
+		if (regex_search(line, pat))
+			 matches.push_back(line);
 
-	for (const auto& line : matches) cout << line << '\n';
+	for (const auto& line : matches)
+		cout << line << '\n';
 
 	return 0;
 }
