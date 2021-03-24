5229d8b0a5816ff7908f5e31c75a8537d99022c0
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-23 20:10:59 -0500

    Update Patron.cpp

--- Chapter09/Exercise_05_06_07_08_09/Patron.cpp
+++ Chapter09/Exercise_05_06_07_08_09/Patron.cpp
@@ -16,105 +16,111 @@
 //----------------------------------------------------------------------------------------------------------------------------------
 #include "Patron.h"
 
-namespace Patrons
-{
-	using std::string;
+using namespace std;
 
-	//------------------------------------------------------------------------------------------------------------------------------
-	// Patron::Patron(const string& full_name_, int account_number_)
-	//------------------------------------------------------------------------------------------------------------------------------
-	/*
-	* A patron is created by having a full name and valid 6 digit account number.
-	* 
-	* @param <full_name_>      input first name and last name
-	* @param <account_number_> input a 6 digit account number
-	* 
-	* @exception Invalid_Patron exception is thrown if one or both members are invalid.
-	* 
-	* @note The default constructor is deleted because you cannot have a default person.
-	*/
-	//------------------------------------------------------------------------------------------------------------------------------
-	Patron::Patron(const string& full_name_, int account_number_)
-		:
-		full_name{ full_name_ }, account_number{ account_number_ }
+namespace Local_Library
+{
+	Patron::Patron() : full_name{ default_patron().get_full_name() },
+		               lib_card_num{ default_patron().get_lib_card_num() }
 	{
-		if (!is_valid_patron(full_name_, account_number_)) { throw Invalid_Patron{}; }
 	}
 
-	//-----------------------------------------------------
-	// Helper functions
-	//-----------------------------------------------------
-
-	//------------------------------------------------------------------------------------------------------------------------------
-	// bool is_valid_patron(const string& full_name_, int account_number_)
-	//------------------------------------------------------------------------------------------------------------------------------
-	/*
-	* This function validates the input for the constructor.
-	*
-	* @param <full_name_>      input first name and last name
-	* @param <account_number_> input a 6 digit account number
-	*
-	* @note test: 1) full_name_ 2) account_number_
-	*/
-	//------------------------------------------------------------------------------------------------------------------------------
-	bool is_valid_patron(const string& full_name_, int account_number_)
+	Patron::Patron(const string& full_name_, const string& lib_card_num_)
+		: full_name{ full_name_ }, lib_card_num{ lib_card_num_ }
 	{
-		constexpr int min_name_len{ 5 };
+		if (!is_valid_patron(full_name_, lib_card_num_)) { throw Invalid_Patron{}; }
+	}
 
-		if (full_name_.length() < min_name_len) { return false; }
+	const Patron& default_patron()
+	{
+		static Patron patron{ "Unknown Name", "000000" };
+		return patron;
+	}
 
-		constexpr int acc_num_len{ 6 };
-		string acc_num_str = std::to_string(account_number_);
+	bool is_valid_patron(const string& full_name, const string& lib_card_num)
+	{
+		if (full_name.length() < Patron::MIN_NAME_LEN) { return false; }
+		if (lib_card_num.length() != Patron::CARD_NUM_LEN) { return false; }
 
-		if (acc_num_str.length() != acc_num_len) { return false; }
+		regex name_pat{ R"([A-Z][a-z]+ [A-Z][a-z]+)" };
+		if (!regex_match(full_name, name_pat)) { return false; }
+
+		regex num_pat{ R"([0-9]{6})" };
+		if (!regex_match(lib_card_num, num_pat)) { return false; }
 
 		return true;
 	}
 
-	//------------------------------------------------------------------------------------------------------------------------------
-	// bool owes_fees(const Patron& patron)
-	//------------------------------------------------------------------------------------------------------------------------------
-	/*
-	* This function checks whether a patron owes fees or not.
-	*
-	* @param <patron> input valid patron
-	*/
-	//------------------------------------------------------------------------------------------------------------------------------
 	bool owes_fees(const Patron& patron)
 	{
-		constexpr double zero_balance{ 0 };
-		if (patron.get_library_fees() == zero_balance) { return false; }
-
+		if (patron.get_lib_fees() == patron.ZERO_BAL) { return false; }
 		return true;
 	}
 
-	//------------------------------------------------------------------------------------------------------------------------------
-	// bool operator==(const Patron& obj1, const Patron& obj2)
-	//------------------------------------------------------------------------------------------------------------------------------
-	/*
-	* Overloading the == (equality) operator to compare patrons.
-	*
-	* @param <obj1> input valid patron
-	* @param <obj2> input valid patron
-	*/
-	//------------------------------------------------------------------------------------------------------------------------------
-	bool operator==(const Patron& obj1, const Patron& obj2)
+	ostream& operator<<(ostream& os, const Patron& patron)
 	{
-		return obj1.get_account_number() == obj2.get_account_number();
+		return os
+			<< "\n[Patron]\n"
+			<< "[Name: " << patron.get_full_name() << "]\n"
+			<< "[LC #: " << patron.get_lib_card_num() << "]\n"
+			<< "[Fees owed: $" << patron.get_lib_fees() << "]\n";
 	}
 
-	//------------------------------------------------------------------------------------------------------------------------------
-	// bool operator!=(const Patron& obj1, const Patron& obj2)
-	//------------------------------------------------------------------------------------------------------------------------------
-	/*
-	* Overloading the != (inequality) operator to compare patrons.
-	*
-	* @param <obj1> input valid patron
-	* @param <obj2> input valid patron
-	*/
-	//------------------------------------------------------------------------------------------------------------------------------
-	bool operator!=(const Patron& obj1, const Patron& obj2)
+	istream& operator>>(istream& is, Patron& patron)
 	{
-		return !(obj1.get_account_number() == obj2.get_account_number());
+		string full_name = name_input(is);
+		string lib_card_num = lcn_input(is);
+
+		patron = Patron{ full_name, lib_card_num };
+
+		return is;
+	}
+
+	bool operator==(const Patron& p1, const Patron& p2)
+	{
+		return p1.get_lib_card_num() == p2.get_lib_card_num();
+	}
+
+	bool operator!=(const Patron& p1, const Patron& p2)
+	{
+		return p1.get_lib_card_num() != p2.get_lib_card_num();
+	}
+
+	string name_input(istream& is)
+	{
+		string first_name, last_name;
+		cout << "[Full name]: ";
+
+		is >> first_name >> last_name;
+
+		if (is.eof())
+		{
+			is.clear();
+			throw Patron::Invalid_Patron{};
+		}
+
+		if (!is)
+		{
+			is.clear();
+			is.ignore();
+			throw Patron::Invalid_Patron{};
+		}
+
+		return first_name + ' ' + last_name;
+	}
+
+	string lcn_input(istream& is)
+	{
+		string lib_card_num;
+		cout << "[LC #]: ";
+		is >> lib_card_num;
+
+		if (is.eof())
+		{
+			is.clear();
+			throw Patron::Invalid_Patron{};
+		}
+
+		return lib_card_num;
 	}
 }
