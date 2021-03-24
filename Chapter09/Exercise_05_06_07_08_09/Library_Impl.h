187e0f8c1cff76770dd66e745fd97ec835fc1fe7
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-23 20:10:06 -0500

    Update Library_Impl.h

--- Chapter09/Exercise_05_06_07_08_09/Library_Impl.h
+++ Chapter09/Exercise_05_06_07_08_09/Library_Impl.h
@@ -19,9 +19,15 @@
 #include "Patron.h"
 #include "Library.h"
 
-namespace Driver
+#include <iostream>
+#include <string>
+#include <exception>
+
+namespace Local_Library
 {
-	void driver();
+	void print_lib(const Library& lib);
+	void driver(const std::string& inv_b, const std::string& inv_p, const std::string& inv_l);
+	char cont_loop(const std::string& prompt);
 }
 
 #endif // LIBRARY_IMPL_H
