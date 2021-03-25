d34280a02581fde569fcd0641cafcd0a37e63faf
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-25 16:23:02 -0500

    Update and rename main.h to Main.h

---
+++ Chapter09/Exercise_14/Main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#pragma once
+
+#include "Money.h"
+
+void driver();
+
+#include <iostream>
+#include <exception>
+
+#endif // MAIN_H
--- Chapter09/Exercise_14/main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <exception>
-#include "Money.h"
-
-void run_program();
