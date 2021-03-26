ab76700eaee42382d584c26ab18cb269bb72473f
Hernan Teran <70241590+HernanTeran@users.noreply.github.com>
2021-03-26 06:26:10 -0500

    Update Exercise_01.cpp

--- Chapter19/Exercise_01.cpp
+++ Chapter19/Exercise_01.cpp
@@ -8,36 +8,38 @@
 using namespace std;
 
 template <typename T, typename U>
-vector<T> f(const vector<T>& vt, const vector<U>& vu);
+void f(vector<T>& vt, const vector<U>& vu);
 
 int main()
 {
-	vector<int> vt, vu, vf;
+	vector<int> vt, vu;
 
 	for (int i{ 0 }; i != 100; ++i) vt.push_back(i);
 	for (int i{ 100 }; i != 200; ++i) vu.push_back(i);
 
-	vf = f(vt, vu);
+	for (const int x : vt)
+		cout << "old: " << x << '\n';
+	cout << endl;
 
-	for (const int x : vf)
-		cout << x << '\n';
+	f(vt, vu);
+
+	for (const int x : vt)
+		cout << "new: " << x << '\n';
 
 	return 0;
 }
 
-/// @brief Adds the elemnts of one vector<U> to another vector<T>
+/// @brief Adds the elements of one vector<U> to another vector<T>
 /// @tparam T 'T'  any numerical element 
 /// @tparam U 'U'  any numerical element
 /// @param vt 'vt' a vector of type T
 /// @param vu 'vu' a vector of type U
-/// @return   'vf' a vector of type T containing vt[i] += vu[i] 
+/// @return Elements are calculated into 'vt' by reference 
 template <typename T, typename U>
-vector<T> f(const vector<T>& vt, const vector<U>& vu)
+void f(vector<T>& vt, const vector<U>& vu)
 {
-	vector<T> vf;
+	if (vt.size() != vu.size()) return;
 
 	for (T i{ 0 }; i != vt.size(); ++i)
-		vf.push_back(vt.at(i) + vu.at(i));
-
-	return vf;
+		vt.at(i) += vu.at(i);
 }
